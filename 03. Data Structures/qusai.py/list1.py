705ee1ab8c906fcfb288c12e1d3004575999b541
Qossay Mohammad Katbeh <22310116@scme.edu.ps>
2024-11-05 08:23:24 +0200

    Update list1.py

--- 03. Data Structures/qusai.py/list1.py
+++ 03. Data Structures/qusai.py/list1.py
@@ -5,7 +5,9 @@
 # print(sum(list1))
 # import random
 # random.shuffle(list1)
+
 #nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+
 # list1 = [2,3]
 # list2 = [1,9]
 # list3 = list1 + list2
@@ -14,7 +16,37 @@
 # print(list3)
 # list4 = list3[2:4]
 # print(list4)
+
 #ttttttttttttttttttttttttttttttttttttttttttttttttttt
-list1= [2,3,5,2,33,21]
-list1[-1]
-print(list1[-1])
+
+# list1= [2,3,5,2,33,21]
+# list1[-1]
+# print(list1[-1])
+
+#33333333333333333333333333333333333333333333333333333
+
+# list1 =[2,3,5,2,33,21]
+# i = 0 
+# while i < len(list1):
+#     print(list1[i],end=" ")
+#     i += 1
+
+#44444444444444444444444444444444444444444444444444444
+
+list1 =[x for x in range (0,12594)]
+print (list1)
+
+list2 = [0.5*x for x in list1]
+print(list2)
+
+list3 = [x for x in list2 if x <1.5]
+print(list3)
+
+
+
+
+
+
+
+
+
