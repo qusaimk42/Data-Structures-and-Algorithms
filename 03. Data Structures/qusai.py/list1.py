c838cb7c8d474c815d7b34c8db4a630cea0451d6
Qossay Mohammad Katbeh <22310116@scme.edu.ps>
2024-11-17 10:50:18 +0200

    Update list1.py

--- 03. Data Structures/qusai.py/list1.py
+++ 03. Data Structures/qusai.py/list1.py
@@ -6,7 +6,7 @@
 # import random
 # random.shuffle(list1)
 
-#nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+#nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
 
 # list1 = [2,3]
 # list2 = [1,9]
@@ -17,13 +17,13 @@
 # list4 = list3[2:4]
 # print(list4)
 
-#ttttttttttttttttttttttttttttttttttttttttttttttttttt
+#tttttttttttttttttttttttttttttttttttttttttttttttttt
 
 # list1= [2,3,5,2,33,21]
 # list1[-1]
 # print(list1[-1])
 
-#33333333333333333333333333333333333333333333333333333
+#33333333333333333333333333333333333333333333333333
 
 # list1 =[2,3,5,2,33,21]
 # i = 0 
@@ -31,7 +31,7 @@
 #     print(list1[i],end=" ")
 #     i += 1
 
-#44444444444444444444444444444444444444444444444444444
+#44444444444444444444444444444444444444444444444444
 
 # list1 =[x for x in range (0,5)]
 # print (list1)
@@ -42,7 +42,7 @@
 # list3 = [x for x in list2 if x <1.5]
 # print(list3)
 
-#5555555555555555555555555555555555555555555555555555
+#5555555555555555555555555555555555555555555555555
 
 # list1 =["pink","red","blue" ]
 # list2 =["green","red and" ,"blue","green"]
@@ -74,13 +74,13 @@
 # list1 =[1,2,3,4,5,6]
 # list2 =reverse(list1)
 # print (list2)
-#====================================================
+#===================================================
 
 # list3 =[10,20,30,40,50,60]
 # list3.reverse()
 # print (list3)
 
-#22222222222222222222222222222222222222222222222222222
+#222222222222222222222222222222222222222222222222222
 
 #def  linearshearch(lst,key):
 #     for i in range(0,len(lst)):
@@ -90,7 +90,7 @@
 # list =[2,3,4,5]
 # print(linearshearch(list,5))
 
-##================ new day ============================================ new day ====================================================== new day ================================
+##================ new day ============================================ new day ====================================================== new day ===============================
 #                                      2024\11\12                                                   2024\11\12                                                               =
 #================ new day ============================================ new day ====================================================== new day ================================
 
@@ -105,12 +105,40 @@
 # selectionsort(lest4)
 # print(lest4)
 
-#22222222222222222222222222222222222222222222222222222222222222222222222222
+#222222222222222222222222222222222222222222222222
 
 # list2=[1,2,5,6,8,5,99,0]
 # list2.sort()
 # print(list2)
 
-#3333333333333333333333333333333333333333333333333333333333333333333333333
+##================ new day ============================================ new day ====================================================== new day ===============================
+#                                      2024\11\17                                                   2024\11\17                                                               =
+#================ new day ============================================ new day ====================================================== new day ================================ 
 
+# total = 0
+# import random
+# matrix =[]
+# numberOFcolumns =eval(input("Enter the number of colums: "))
+# numberOFRows =eval(input("Enter the number of Rows: "))
+# for row in range(0,numberOFRows):
+#     matrix.append([])
+#     for column in range(0,numberOFcolumns):
+#         matrix[row].append(random.randrange(0,100))
+#         total  +=matrix[row][column]
+                          
+# print("Total is 2 is" + str(total))
+# print(matrix)
 
+#222222222222222222222222222222222222222222222222
+
+#matrix=[]
+# NumOfRows=eval(input("Enter the number of rows : "))
+# NumOfcols=eval(input("Enter the number of cols : "))
+
+# for row in range(0,NumOfRows):
+#     matrix.append([])
+#     for Cols in range(0,NumOfcols):
+#         print("Row#",row+1 , " Cloumn# " , colum +1)
+#         value=eval(input("Enter the element and enter "))
+#         matrix[row].append(value)
+# print(matrix)
